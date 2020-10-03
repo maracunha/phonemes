52f927582b76b0a5d925647bc4d399d864c557aa
Renan Marangon <maracunha@gmail.com>
2020-10-03 17:04:37 -0300

    feat: 😜 randam list of animations

--- src/components/Animation/index.js
+++ src/components/Animation/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import heart from '../../assets/animations/love-hearts.json'
 
+const animations = {
+  school: require('../../assets/animations/back-to-scool.json'),
+  bell: require('../../assets/animations/bell-icon.json'),
+  girl: require('../../assets/animations/girl-on-the-bicycle.json'),
+  heart: require('../../assets/animations/love-hearts.json'),
+  job: require('../../assets/animations/love-your-job.json'),
+  love: require('../../assets/animations/love.json'),
+  illustration: require('../../assets/animations/nepenthe-illustration.json'),
+}
+
+const animationsLength = Object.keys(animations).length
+const randomKeyAnimation = Math.floor(Math.random() * animationsLength)
+const currentAnimation = Object.keys(animations)[randomKeyAnimation]
 
 function Animation () {
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: heart,
+    animationData: animations[currentAnimation],
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
     }
